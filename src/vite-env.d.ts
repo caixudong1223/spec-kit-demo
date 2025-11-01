41fb1d64a5527e6865056162b1d6fb3c51c8699f
caixd_sr <caixd_sr@neusiri.com>
2025-11-01 17:28:45 +0800

    添加对 SVG 文件的模块声明，以支持不同类型的 SVG 导入，包括默认、URL 和原始格式。

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,17 @@
 /// <reference types="vite/client" />
+
+// SVG imports
+declare module '*.svg' {
+  const content: string
+  export default content
+}
+
+declare module '*.svg?url' {
+  const content: string
+  export default content
+}
+
+declare module '*.svg?raw' {
+  const content: string
+  export default content
+}
